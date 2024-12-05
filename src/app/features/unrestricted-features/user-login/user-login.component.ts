bfc9fb12ea3bc8a13229bfcb7d8206a211c581bf
Agiratech-Kiruthika <kiruthika.p@agiratech.com>
2024-12-05 17:05:48 +0530

    feat:Add the client side validation

--- src/app/features/unrestricted-features/user-login/user-login.component.ts
+++ src/app/features/unrestricted-features/user-login/user-login.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginForm } from '../../../models/forms/login.form';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule,CommonModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.scss'
 })
-export class UserLoginComponent {
+export class UserLoginComponent  implements OnInit {
+  loginForm!: FormGroup<LoginForm>;
+  passwordVisible: boolean = false;
+  errorMessages: { [key: string]: string } = {};
+
+    
+  constructor(
+      private fb: FormBuilder
+    ) {
+    
+
+      }
+  
+    ngOnInit() {
+      this.initializeFormData();
+  
+      this.loginForm.valueChanges.subscribe(() => {
+        this.setErrorMessages();
+      });
+    }
+  
+
+  initializeFormData() {
+    this.loginForm = this.fb.group<LoginForm>(new LoginForm());
+  }
+
+  onSubmit() {
+     this.loginForm.markAllAsTouched();
+     if(this.loginForm.invalid){
+      return
+     }
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  private setErrorMessages() {
+    this.errorMessages = {};
+  
+    const controls = {
+      email: this.loginForm.get('email'),
+      password: this.loginForm.get('password'),
+    };
+  
+    Object.entries(controls).forEach(([field, control]) => {
+      if (control?.invalid && (control.dirty || control.touched)) {
+        if (control.hasError('required')) {
+          this.errorMessages[field] = `${this.capitalize(field)} is required.`;
+        }
+      }
+    });
+  }
+  
+  private capitalize(word: string): string {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+  
 
 }
---
+++ src/app/models/forms/login.form.ts
@@ -0,0 +1,18 @@
+import { FormControl, Validators } from '@angular/forms';
+
+export interface ILoginForm {
+  email: FormControl<string | null >;
+  password: FormControl<string | null>;
+}
+
+export class LoginForm implements ILoginForm {
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+}
